378b3b32eaf3580a2133303245954a429f7963e7
Ryan <ryanbelt1993129@Hotmail.com>
2016-04-25 15:21:51 -0400

    nothing new

--- app/src/main/java/ryanbelt/utscroom/MainActivity.java
+++ app/src/main/java/ryanbelt/utscroom/MainActivity.java
@@ -2,6 +2,8 @@ package ryanbelt.utscroom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button EmptyRoomButton = (Button)findViewById(R.id.EmptyRoomButton);
+
+       // EmptyRoomButton.setOnClickListener();
     }
 }
