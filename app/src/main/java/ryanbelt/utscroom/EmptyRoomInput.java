9bbf67c11dc0465519e8179bbeee2903525a679e
Ryan <ryanbelt1993129@Hotmail.com>
2016-04-25 23:22:56 -0400

    added emptyroom select page

---
+++ app/src/main/java/ryanbelt/utscroom/EmptyRoomInput.java
@@ -0,0 +1,16 @@
+package ryanbelt.utscroom;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ryan on 2016-04-25.
+ */
+public class EmptyRoomInput extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_empty_room_input);
+    }
+}
--- app/src/main/java/ryanbelt/utscroom/MainActivity.java
+++ app/src/main/java/ryanbelt/utscroom/MainActivity.java
@@ -1,5 +1,6 @@
 package ryanbelt.utscroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button EmptyRoomButton = (Button)findViewById(R.id.EmptyRoomButton);
+        Button RoomScheduleButton = (Button)findViewById(R.id.RoomSchedule);
 
+        EmptyRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,EmptyRoomInput.class));
+            }
+        });
        // EmptyRoomButton.setOnClickListener();
     }
 }
