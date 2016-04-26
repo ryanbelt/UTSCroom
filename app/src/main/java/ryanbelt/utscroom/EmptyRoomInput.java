674acb504a48c88002a6160d620dc69c9561e65c
Ryan <ryanbelt1993129@Hotmail.com>
2016-04-25 23:58:06 -0400

    emptyroom page select feature done

--- app/src/main/java/ryanbelt/utscroom/EmptyRoomInput.java
+++ app/src/main/java/ryanbelt/utscroom/EmptyRoomInput.java
@@ -3,14 +3,38 @@ package ryanbelt.utscroom;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 /**
  * Created by Ryan on 2016-04-25.
  */
 public class EmptyRoomInput extends AppCompatActivity {
+    private Button startButton;
+    private Spinner daySpinner;
+    private TimePicker timePick;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_empty_room_input);
+
+        daySpinner = (Spinner)findViewById(R.id.DaySpinner);
+        timePick = (TimePicker)findViewById(R.id.TimePicker);
+        startButton = (Button)findViewById(R.id.StartButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int hour = timePick.getCurrentHour();
+                int min = timePick.getCurrentMinute();
+                int day = daySpinner.getSelectedItemPosition()-1;
+                Log.d("myCheck",String.format("%d   %d:%d", day, hour, min));
+            }
+        });
     }
 }
